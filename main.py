b58162c1f05ebfc91f4bd464387d262ac19ab207
G4B4-ops <gabrielsturm4@gmail.com>
2026-09-22 14:06:38 +0200

    Replace yfinance with random price generation
    Replaced yfinance with random price generation and added calculations for returns, market variance, and beta.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import yfinance as yf
-tickers = [["AAPL"]]
-prices = yf.download(tickers, start="2020-01-01", end="2021-01-01"  )
-print(prices.head())
+import random
+
+prices = [random.randint(50, 75) for _ in range(100)]
+
+print(prices)
+returns = [((prices[i] - prices[i - 1]) / prices[i - 1]) * 100 for i in range(1, len(prices))]
+print(returns)
+import random
+Market = [random.randint(-20, 12) for _ in range(len(returns))]
+print(Market)
+import statistics
+market_variance = statistics.variance(Market)
+print(market_variance)
+statistics.covariance(returns, Market)
+print(statistics.covariance(returns, Market))
+Beta = statistics.covariance(returns, Market) / market_variance
+print(Beta)
